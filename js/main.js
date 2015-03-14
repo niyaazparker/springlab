77da2d4f1a6be5deb3b3d2558fab555c874617b4
Niyaaz <niyaazparker@gmail.com>
2015-03-14 23:24:13 +0200

    web ap stytling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // Load Video
 function loadVideo(playerUrl, autoplay) {
   swfobject.embedSWF(
-      playerUrl + '&rel=1&border=0&fs=1&showinfo=0&autohide=1&autoplay=' + 
+      playerUrl + '&rel=1&border=0&fs=1&showinfo=1&autohide=1&autoplay=' + 
       (autoplay?1:0), 'player', '450', '250', '9.0.0', false, 
       false, {allowfullscreen: 'true'});
 }
+
 
 // Get feed data and push thumbnails 
 function showVideoList(data) {
@@ -19,14 +20,19 @@ function showVideoList(data) {
     var playerUrl = entries[i].media$group.media$content[0].url;
     var viewcount = entry.yt$statistics.viewCount;
     html.push('<li class="col-lg-12 col-md-12 col-xs-12"  onclick="loadVideo(\'', playerUrl, '\', true)">',
-              '<div class="col-lg-4 col-md-4 col-xs-4"><img src="', 
-              thumbnailUrl, '" width="130" height="97"/></div><div class="col-lg-8 col-md-8 col-xs-8"><span class="titlec">', title, '...</span><br><span class="views">Views: ', viewcount,'</span><span class="views">', description,'</span></li></div>');
+              '<div class="col-lg-2 col-md-2 col-xs-12"><img src="', 
+              thumbnailUrl, '" width="130" height="97"/></div><div class="col-lg-10 col-md-10 col-xs-12"><h3 class="titlec">', title, '...</h3><span class="views col-lg-12 col-md-12 col-xs-12">Views: ', viewcount,'</span><span class="description col-lg-12 col-md-12 col-xs-12">', description,'</span></li></div>');
   }
+
+
   html.push('</ul><br style="clear: left;"/>');
   document.getElementById('videoList').innerHTML = html.join('');
 
   // Load first video by default
   if (entries.length > 0) {
     loadVideo(entries[0].media$group.media$content[0].url, false);
+
   }
 }
+
+
