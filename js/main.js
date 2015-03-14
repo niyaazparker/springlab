8e8aaa3422c710ff43e0e08432be5177c3d08e00
Niyaaz <niyaazparker@gmail.com>
2015-03-14 13:24:19 +0200

    jquery files updated

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Load Video
+function loadVideo(playerUrl, autoplay) {
+  swfobject.embedSWF(
+      playerUrl + '&rel=1&border=0&fs=1&showinfo=0&autohide=1&autoplay=' + 
+      (autoplay?1:0), 'player', '450', '250', '9.0.0', false, 
+      false, {allowfullscreen: 'true'});
+}
+
+// Get feed data and push thumbnails 
+function showVideoList(data) {
+  var feed = data.feed;
+  var entries = feed.entry || [];
+  var html = ['<ul class="videos">'];
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    var title = entry.title.$t.substr(0, 20);
+    var description = entries[i].media$group.media$description.$t.substr(0, 100);
+    var thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
+    var playerUrl = entries[i].media$group.media$content[0].url;
+    var viewcount = entry.yt$statistics.viewCount;
+    html.push('<li class="col-lg-12 col-md-12 col-xs-12"  onclick="loadVideo(\'', playerUrl, '\', true)">',
+              '<div class="col-lg-4 col-md-4 col-xs-4"><img src="', 
+              thumbnailUrl, '" width="130" height="97"/></div><div class="col-lg-8 col-md-8 col-xs-8"><span class="titlec">', title, '...</span><br><span class="views">Views: ', viewcount,'</span><span class="views">', description,'</span></li></div>');
+  }
+  html.push('</ul><br style="clear: left;"/>');
+  document.getElementById('videoList').innerHTML = html.join('');
+
+  // Load first video by default
+  if (entries.length > 0) {
+    loadVideo(entries[0].media$group.media$content[0].url, false);
+  }
+}
